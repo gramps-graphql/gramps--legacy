88eaf2f53ba5522806a0a3b7a16dcd5f79e5da6c
Eric Wyne <ecwyne@gmail.com>
2017-11-01 12:32:07 -0700

    update tests

--- test/gramps.test.js
+++ test/gramps.test.js
@@ -1,9 +1,4 @@
 import gramps from '../src/gramps';
-import * as cfg from '../src/lib/configureSchema';
-
-// Stub out the functions that are tested elsewhere.
-cfg.getSchema = jest.fn(opts => opts);
-cfg.addMockFunctions = jest.fn();
 
 describe('GrAMPS', () => {
   beforeEach(() => {
@@ -11,30 +6,6 @@ describe('GrAMPS', () => {
   });
 
   describe('gramps()', () => {
-    it('properly configures a schema when called with no options', () => {
-      const grampsConfig = gramps();
-
-      expect(cfg.addMockFunctions).toHaveBeenCalled();
-      expect(cfg.getSchema).toHaveBeenCalledWith({
-        sources: [],
-        logger: console,
-        options: {},
-      });
-
-      expect(grampsConfig).toEqual(
-        expect.objectContaining({
-          context: {},
-          schema: expect.any(Object),
-        }),
-      );
-    });
-
-    it('does not add mock functions when the enableMockData flag is false', () => {
-      gramps({ enableMockData: false });
-
-      expect(cfg.addMockFunctions).not.toHaveBeenCalled();
-    });
-
     it('properly combines contexts', () => {
       const dataSources = [
         { namespace: 'Foo', model: { foo: 'test' } },
