84d9870cdf3dced608da5ffb2d1a2cee447c1490
Eric Wyne <ecwyne@gmail.com>
2017-12-13 16:09:58 -0800

    make mapResolvers a pure function

--- src/lib/mapResolvers.js
+++ src/lib/mapResolvers.js
@@ -1,6 +1,8 @@
 export default (namespace, resolvers) => {
   if (resolvers instanceof Object) {
+    const mappedResolvers = {};
     Object.keys(resolvers).forEach(type => {
+      mappedResolvers[type] = {};
       Object.keys(resolvers[type]).forEach(field => {
         const fn = resolvers[type][field];
         if (typeof fn !== 'function') {
@@ -8,10 +10,10 @@ export default (namespace, resolvers) => {
             `Expected Function for ${type}.${field} resolver but received ${typeof fn}`,
           );
         }
-        resolvers[type][field] = (root, args, context, info) =>
+        mappedResolvers[type][field] = (root, args, context, info) =>
           fn(root, args, context[namespace], info);
       });
     });
-    return resolvers;
+    return mappedResolvers;
   }
 };
