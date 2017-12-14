f24518a41755bffa4f3a7717ca6a9c867ea2da90
Mike Fix <mrfix84@gmail.com>
2017-12-13 18:26:22 -0800

    Update `mapResolvers` to address concerns

--- src/lib/mapResolvers.js
+++ src/lib/mapResolvers.js
@@ -1,28 +1,25 @@
-import merge from 'lodash.merge';
+const mapObj = fn => obj =>
+  Object.keys(obj).reduce(
+    (acc, key) => ({ ...acc, [key]: fn(obj[key], key, obj) }),
+    {},
+  );
+
+const checkFn = (fn, fieldName) => {
+  if (typeof fn !== 'function') {
+    throw new Error(
+      `Expected Function for ${fieldName} resolver but received ${typeof fn}`,
+    );
+  }
+};
+
+const wrapFn = namespace => (fn, fieldName) => {
+  checkFn(fn, fieldName);
+  return (root, args, context, info) =>
+    fn(root, args, context[namespace], info);
+};
 
 export default (namespace, resolvers) => {
   if (resolvers instanceof Object) {
-    return Object.keys(resolvers).reduce((newResolvers, type) => {
-      const fieldResolvers = Object.keys(resolvers[type]).map(field => {
-        const fn = resolvers[type][field];
-
-        if (typeof fn !== 'function') {
-          throw new Error(
-            `Expected Function for ${type}.${field} resolver but received ${typeof fn}`,
-          );
-        }
-
-        const resolver = (root, args, context, info) =>
-          fn(root, args, context[namespace], info);
-
-        return {
-          [type]: {
-            [field]: resolver,
-          },
-        };
-      });
-
-      return merge(newResolvers, ...fieldResolvers);
-    }, {});
+    return mapObj(mapObj(wrapFn(namespace)))(resolvers);
   }
 };
