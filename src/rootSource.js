9bf76aff9b63e20b8eed704af1314abbf956809d
Ryan Mackey <ryan.o.mackey@gmail.com>
2018-10-16 13:05:39 -0500

    feat: add Upload scalar/resolver (#107)
    This PR adds file upload support to GrAMPS by adding a new `Upload` scalar and resolver: https://github.com/jaydenseric/graphql-upload
    
    Further reading: https://www.apollographql.com/docs/guides/file-uploads.html

--- src/rootSource.js
+++ src/rootSource.js
@@ -1,6 +1,9 @@
+import { GraphQLUpload } from 'graphql-upload/lib/GraphQLUpload';
 import pkg from '../package.json';
 
 const typeDefs = `
+  scalar Upload
+
   type Query {
     # Returns the current version of GrAMPS.
     grampsVersion: String!
@@ -12,6 +15,8 @@ const typeDefs = `
 `;
 
 const resolvers = {
+  Upload: GraphQLUpload,
+
   Query: {
     grampsVersion: /* istanbul ignore next */ () => pkg.version,
   },
